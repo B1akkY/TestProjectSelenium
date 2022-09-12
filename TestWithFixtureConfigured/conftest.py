d8c511fd60d6559e09f6fb86a2bb6b1b0305890a
B1ackY <756478@gmail.com>
2022-09-12 06:45:40 +0300

    fixture update

--- TestWithFixtureConfigured/conftest.py
+++ TestWithFixtureConfigured/conftest.py
@@ -3,8 +3,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+
+
 @pytest.fixture(scope="function")
-def browser():
-    browser = webdriver.Chrome()
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        browser = webdriver.Chrome()
+        browser.maximize_window()
+    elif browser_name == "firefox":
+        browser = webdriver.Firefox()
+        browser.maximize_window()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     browser.quit()
